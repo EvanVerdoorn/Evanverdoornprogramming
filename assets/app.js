9fc17611372218fa7fcc2658d5dfb54f4b905559
EvanVerdoorn <54663770+EvanVerdoorn@users.noreply.github.com>
2019-09-18 11:40:53 +0200

    koffieeee

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// opdracht 1
+let x = 2;
+let y = 6;
+console.log(y % x)
+
+//opdracht 2
+let sentence = " Programming is not so cool "
+console.log(sentence.replace(" not ", " "))
+
+//opdracht 3
+const p = 1400
+const q = "ik woon in naboo"
+console.log(p == q)
+// het is niet handig, want de waarde '1400' bestaat niet in 'ik woon in naboo'
