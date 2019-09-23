a1e3f922f43e97343d8128f251cf6a5d7c256c48
EvanVerdoorn <54663770+EvanVerdoorn@users.noreply.github.com>
2019-09-23 08:31:15 +0200

    opdracht

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+//types and variables
+
 // opdracht 1
 let x = 2;
 let y = 6;
@@ -12,3 +14,16 @@ const p = 1400
 const q = "ik woon in naboo"
 console.log(p == q)
 // het is niet handig, want de waarde '1400' bestaat niet in 'ik woon in naboo'
+
+
+//conditionals
+
+//opdracht 1
+let cijfer = "7";
+if (cijfer < 6) {console.log("onvoldoende")};
+else if (cijfer >= 6 && cijfer <= 7 ) {console.log("voldoende")};
+else if (cijfer >= 7 && cijfer <= 9 ) {console.log("goed")};
+else if (cijfer > 9) {console.log("uitmuntend")};
+
+//opdracht 2
+
