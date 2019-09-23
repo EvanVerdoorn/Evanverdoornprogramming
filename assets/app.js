75b6cf4fc49c04923a03e7dcb0e36874ad195c73
EvanVerdoorn <54663770+EvanVerdoorn@users.noreply.github.com>
2019-09-23 08:56:50 +0200

    opdracht

--- assets/app.js
+++ assets/app.js
@@ -19,11 +19,36 @@ console.log(p == q)
 //conditionals
 
 //opdracht 1
-let cijfer = "7";
-if (cijfer < 6) {console.log("onvoldoende")};
-else if (cijfer >= 6 && cijfer <= 7 ) {console.log("voldoende")};
-else if (cijfer >= 7 && cijfer <= 9 ) {console.log("goed")};
-else if (cijfer > 9) {console.log("uitmuntend")};
+let cijfer = "6";
+if (cijfer < 6) {console.log("onvoldoende")}
+else if (cijfer >= 6 && cijfer <= 7 ) {console.log("voldoende")}
+else if (cijfer >= 7 && cijfer <= 9 ) {console.log("goed")}
+else if (cijfer > 9) {console.log("uitmuntend")}
 
 //opdracht 2
+
+var grade = "7"
+
+switch (true) {
+    case grade < 6:
+        console.log("onvoldoende")
+        break;
+    case grade >= 6 && grade <= 7:
+        console.log("voldoende")   
+        break; 
+    case grade >= 7 && cijfer <= 9:
+        console.log("goed")  
+        break;
+    case grade > 9:
+        console.log("uitmuntend")  
+        break;    
+}
+
+//opdracht 3
+
+let purchasedBook = true;
+let job = "teacher";
+let inTrain = false;
+
+if (purchasedBook == true && job == "teacher" && inTrain == true) {console.log("finally I can enjoy my book")}
 
